cea234650c240a48ba1551c56ea6f1e8f8237246
Adam Bergman <adam@fransvilhelm.com>
2020-09-12 15:11:37 +0200

    Create initial implementation

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { ChangelogFunctions } from '@changesets/types';
+import githubChangelogFunctions from '@changesets/changelog-github';
+import { getInfo } from '@changesets/get-github-info';
+
+const changelogFunctions: ChangelogFunctions = {
+  ...githubChangelogFunctions,
+  getReleaseLine: async (changeset, _, options) => {
+    if (!options || !options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["@fransvilhelm/changeset-changelog", { "repo": "org/repo" }]',
+      );
+    }
+    const [firstLine, ...summary] = changeset.summary
+      .split('\n')
+      .map((l) => l.trimRight());
+
+    if (changeset.commit) {
+      let { links } = await getInfo({
+        repo: options.repo,
+        commit: changeset.commit,
+      });
+
+      let { pull, commit, user } = links;
+      let userInfo = `(by ${user ?? '(unknown)'} in ${pull ?? commit})`;
+
+      let init = `- ${firstLine} ${userInfo}`;
+      let rest = summary.map((l) => `  ${l}`).join('\n');
+      return '\n\n' + [init, rest].filter(Boolean).join('\n');
+    } else {
+      return githubChangelogFunctions.getReleaseLine(changeset, _, options);
+    }
+  },
+};
+
+export default changelogFunctions;
